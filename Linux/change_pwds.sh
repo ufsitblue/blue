3963ae11d7bc5cbb5e8a007092d259c332c5f41c
Maxwell Griffin <92822857+Maxgriff@users.noreply.github.com>
2024-02-04 12:16:21 -0500

    Changed password gen method

--- Linux/change_pwds.sh
+++ Linux/change_pwds.sh
@@ -4,7 +4,9 @@
 
 
 if [[ -z $ROOT_PW ]]; then
-    ROOT_PW=$(echo "$(date +%N)$RANDOM" | sha256sum | cut -d" " -f1 | cut -c -20)
+    #ROOT_PW=$(echo "$(date +%N)$RANDOM" | sha256sum | cut -d" " -f1 | cut -c -20)
+    # Uses /dev/urandom to make passwords more secure
+    ROOT_PW=$(head -c 100 /dev/urandom | tr -dc 'a-zA-Z0-9@#$%&!?:*-+=' | cut -c1-23)
 fi
 
 echo "root:$ROOT_PW" | chpasswd
