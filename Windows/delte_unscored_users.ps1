542fa59b49c34e9229ead3df8f842cbedfa6c586
max <maxwellgriffin@ufl.edu>
2024-02-04 23:37:32 -0500

    Added functionality for Domain users

--- Windows/delte_unscored_users.ps1
+++ Windows/delte_unscored_users.ps1
@@ -27,3 +27,20 @@ foreach ($user in $allUsers) {
         }
     }
 }
+
+# get all domain user accounts
+$allDomainUsers = Get-ADUser -Filter *
+
+foreach ($user in $allDomainUsers) {
+    $username = $user.SamAccountName
+
+    if ($usersToKeep -notcontains $username) {
+	# attempt to delete the user, ignoring any errors
+        try {
+	    Remove-ADUser -Identity $username -Confirm:$false -ErrorAction Stop
+	    Write-Output "Deleted user: $username"
+	} catch {
+	    Write-Output "Could not delete user: $($username). Error: $_"
+	}
+    }
+}
