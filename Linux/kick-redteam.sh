025a929914175e0018fcdc5b4494197adfcbef72
FrightenedWombat862 <nicetry@thisisnotmyrealemail.invalid>
2024-02-16 22:22:51 -0500

    Add new scripts

---
+++ Linux/kick-redteam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Only works on Debian/Ubuntu, you need the iptables-persistent package for this to work
+# Needs to be run as root.
+
+wall nice try buddy
+if [[-v $1]]
+	iptables -I INPUT -s $1 -j DROP
+	iptables -I OUTPUT -d $1 -j DROP
+iptables-save > /etc/iptables/rules.v4
+ip6tables-save > /etc/iptables/rules.v6
+reboot
