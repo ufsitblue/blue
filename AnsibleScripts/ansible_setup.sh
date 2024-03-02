825cd81bc59117d11ce9c4cd15b584e9394acc97
max <maxwellgriffin@ufl.edu>
2024-03-02 10:53:36 -0500

    Added support for bsd hosts

--- AnsibleScripts/ansible_setup.sh
+++ AnsibleScripts/ansible_setup.sh
@@ -9,12 +9,14 @@ fi
 # Define the variables
 inventory_file="$(pwd)/inventory/hosts.yaml"
 linux_file="linux.yaml"
+bsd_file="bsd.yaml"
 win_file="windows.yaml"
 man_file="manager.yaml"
 got_man=0
 private_key=0
 win=0
 linux=0
+bsd=0
 
 # Read in use host file location and store it, otherwise create directory inventory if it doesn't exist
 read -p "Where is your hosts file? (Blank for default: $inventory_file): " user_hosts
@@ -34,6 +36,8 @@ fi
 # Create or overwrite the inventory file
 echo -e "linux:" > "$linux_file"
 echo -e "  hosts:" >> "$linux_file"
+echo -e "bsd:" > "$bsd_file"
+echo -e "  hosts:" >> "$bsd_file"
 echo -e "windows:" > "$win_file"
 echo -e "  hosts:" >> "$win_file"
 echo -e "manager:" > "$man_file"
@@ -44,7 +48,7 @@ echo -e "" > $inventory_file
 
 # Add each host to the inventory file with user-provided details
 for host in "$@"; do
-    read -p "Is '$host' a Linux or Windows host? (l/w): " os_type
+    read -p "Is '$host' a Linux, Windows, or BSD host? (l/w/b): " os_type
     os_type=$(echo "$os_type" | tr '[:upper:]' '[:lower:]')  # Convert to lowercase
     
     case "$os_type" in
@@ -57,6 +61,7 @@ for host in "$@"; do
 	    echo -e "    $host:" >> $linux_file
 	    echo -e "      ansible_host: $ip" >> $linux_file
 	    echo -e "      ansible_user: $user" >> $linux_file
+      echo -e "      in_domain: true" >> $linux_file
      
 	    read -p "Are you using private key login? (y/n): " priv
 	    
@@ -125,6 +130,84 @@ for host in "$@"; do
 	    fi
 	    echo
             ;;
+        b|bsd)
+	    bsd=1
+	    # Gets the ip, ssh username, and the password/private key
+	    read -p "Enter ip address: " ip
+	    read -p "Enter SSH username: " user
+            # Add that infor to file no matter what
+	    echo -e "    $host:" >> $bsd_file
+	    echo -e "      ansible_host: $ip" >> $bsd_file
+	    echo -e "      ansible_user: $user" >> $bsd_file
+      echo -e "      in_domain: true" >> $bsd_file
+     
+	    read -p "Are you using private key login? (y/n): " priv
+	    
+	    if [ "$priv" = "y" ]; then
+			private_key=1
+			while true; do
+				echo -e "Select a private key:\n"
+				for i in "${!used_priv_keys[@]}"; do
+					echo "$((i+1))) ${used_priv_keys[i]}"
+				done
+				echo -e "n) Enter a new private key path\n"
+				read -p "Your choice (number or 'n'): " choice
+
+				# Check if choice is a number and within the range of used keys
+				if [[ "$choice" =~ ^[0-9]+$ ]] && [ "$choice" -le "${#used_priv_keys[@]}" ] && [ "$choice" -gt 0 ]; then
+					priv_path="${used_priv_keys[$((choice-1))]}"
+					echo "Selected private key: $priv_path"
+					break
+				elif [ "$choice" = "n" ]; then
+					read -e -p "Enter new private key path: " priv_path
+					priv_path=$(realpath "$priv_path") # Convert to absolute path
+					if [ -z "$priv_path" ]; then
+						echo "Skipping private key configuration."
+						break
+					elif [ -f "$priv_path" ] && [ -r "$priv_path" ]; then
+						echo "Private key path is valid."
+						used_priv_keys+=("$priv_path") # Add the new key path to the array
+						break
+					else
+						echo "Invalid path or file not readable. Please try again."
+					fi
+				else
+					echo "Invalid choice. Please try again."
+				fi
+			done
+
+			if [ -n "$priv_path" ]; then
+				# Input information into bsd yaml file
+    				echo -e "      ansible_private_key_file: $priv_path" >> $bsd_file
+			fi
+	    else
+	       read -s -p "Enter SSH password" password
+	       echo
+	       
+	       # Input information into bsd yaml file
+	       echo -e "      ansible_password: $password" >> $bsd_file
+	    fi
+
+	    # Check if the current host is the wazuh manager to add it to the manager group
+	    if [ $got_man -eq 0 ]; then
+	       read -p "Is this the Wazuh Manager? (y/n): " ans
+	       if [ $ans = "y" ]; then
+		  # Only have one wazuh manager
+		  got_man=1
+                  echo -e "    $host:" >> $man_file
+	          echo -e "      ansible_host: $ip" >> $man_file
+	          echo -e "      ansible_user: $user" >> $man_file
+                  
+                  # Check if using private key or password for login then input the corresponding info into manager yaml
+		  if [ $private_key -eq "1" ]; then
+	             echo -e "      ansible_private_key_file: $priv_path" >> $man_file
+	          else
+	             echo -e "      ansible_password: $password" >> $man_file
+	          fi
+	       fi
+	    fi
+	    echo
+            ;;
         w|windows)
 	    win=1
 	    # Get the winrm username and password and the ip address to connect to
@@ -132,23 +215,8 @@ for host in "$@"; do
             read -s -p "Enter winrm password: " password
 	    echo
 	    read -p "Enter ip address: " ip
+      read -p "Domain cotroller? (true or false)" controller
 	    
-	    # Check if the current host is the wazuh manager and put info into the appropriate file
-	    if [ $got_man -eq 0 ]; then
-	       read -p "Is this the Wazuh Manager? (y/n): " ans
-	       if [ $ans = "y" ]; then
-		  got_man=1
-                  echo -e "    $host:" >> $man_file
-		  echo -e "      ansible_host: $ip" >> $man_file
-                  echo -e "      ansible_user: $user" >> $man_file
-                  echo -e "      ansible_password: $password" >> $man_file
-		  echo -e '      ansible_connection: "winrm"' >> $man_file
-		  echo -e '      ansible_winrm_scheme: "http"' >> $man_file
-                  echo -e '      ansible_port: "5985"' >> $man_file
-		  echo -e '      ansible_winrm_transport: "ntlm"' >> $man_file
-	       fi
-	    fi
-
             echo -e "    $host:" >> $win_file
 	    echo -e "      ansible_host: $ip" >> $win_file
             echo -e "      ansible_user: $user" >> $win_file
@@ -157,6 +225,8 @@ for host in "$@"; do
 	    echo -e '      ansible_winrm_scheme: "http"' >> $win_file
             echo -e '      ansible_port: "5985"' >> $win_file
 	    echo -e '      ansible_winrm_transport = "ntlm"' >> $win_file
+      echo -e "      in_domain: true" >> $win_file
+      echo -e "      domain_controller: $controller" >> $win_file
             echo
 	    ;;
         *)
@@ -174,17 +244,22 @@ if [ $linux -eq 0 ]; then
     cat /dev/null > $linux_file
 fi
 
+if [ $bsd -eq 0]; then
+    cat /dev/null > $bsd_file
+fi
+
 if [ $got_man -eq 0 ]; then
     cat /dev/null > $man_file
 fi
 # Update/Append the host info into the inventory/hosts file
 # yq ea '. as $item ireduce ({}; . * $item )' $inventory_file $linux_file $man_file $win_file > $inventory_file
 cat $linux_file >> $inventory_file
+cat $bsd_file >> $inventory_file
 cat $win_file >> $inventory_file
 cat $man_file >> $inventory_file
 
 # Remove intermediate files
-rm "$linux_file" "$win_file" "$man_file"
+rm "$linux_file" "$win_file" "$man_file" "$bsd_file"
 echo "Ansible inventory file '$inventory_file' updated successfully."
 echo
 echo "Installing Ansible Packages..."
