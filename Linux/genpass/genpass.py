1f99e89a256dbf2152a01661f456ac8ea6fe5b52
FrightenedWombat862 <115114652+FrightenedWombat862@users.noreply.github.com>
2024-03-08 22:05:06 -0500

    Update scripts again

--- Linux/genpass/genpass.py
+++ Linux/genpass/genpass.py
@@ -1,3 +1,8 @@
+"""
+Script to generate a password on Linux machines
+
+Copyright (c) 2024 Yuliang Huang
+"""
 import os
 import secrets
 import sys
@@ -6,21 +11,18 @@ def printhelp(programname: str):
     print(programname + " <NUMBER_OF_WORDS>")
     print("NUMBER_OF_WORDS - The number of words in the password to generate")
 
-def main(argv: list[str]) -> int:
+def genpass(num_words: int = 4) -> list[str]:
+    """
+    Gets a password of a certain length, as a list of strings
+    :param num_words: Number of words in the password. Default is 4
+    :return: A list of num_words strings.
+    """
     basepath = os.path.abspath(".")
     try:
         basepath = sys._MEIPASS
     except Exception:
         pass
 
-    words = 4
-    if len(argv) >= 2:
-        try:
-            words = int(argv[1])
-        except ValueError:
-            printhelp(argv[0])
-            return 0
-    
     wordlist: list = []
     with open(basepath + os.sep + "WORDLIST.TXT", "r") as wordlistfile:
         while True:
@@ -30,9 +32,25 @@ def main(argv: list[str]) -> int:
             else:
                 wordlist.append(wordlistfile.readline())
     
-    for _ in range(words):
-        print(end=secrets.choice(wordlist).strip() + ' ')
-    print()
+    output_words = []
+    for _ in range(num_words):
+        output_words.append(secrets.choice(wordlist).strip())
+
+    return output_words
+
+def main(argv: list[str]) -> int:
+    
+    words = 4
+    if len(argv) >= 2:
+        try:
+            words = int(argv[1])
+        except ValueError:
+            printhelp(argv[0])
+            return 0
+    
+    output_words = genpass(words)
+
+    print(' '.join(output_words))
 
     return 0
 
