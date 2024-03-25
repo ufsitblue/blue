14b1843f1142862c932e25a827fb60ec2c7a94b1
max <maxwellgriffin@ufl.edu>
2024-03-24 23:50:05 -0400

    Made compatible with bsd

--- EarlyScripts/Linux/change_pwds.sh
+++ EarlyScripts/Linux/change_pwds.sh
@@ -1,25 +1,36 @@
 # Change passwords.
-passChanger=chpasswd
+bsd=0
 
 cat /etc/os-release | grep -i "bsd" > /dev/null
 if [ $? -eq 0 ]; then
-  passChanger=chpass
+  bsd=1
 fi
 
 users=""
 read -p "Enter list of protected users space delimited: " special
 
-# Change password for users
-for user in $(cat /etc/passwd | grep "sh$" | cut -d ':' -f 1); do
-    # Check if user exists
+if [ $bsd -eq 1 ]; then
+  for user in $(cat /etc/passwd | grep "sh$" | cut -d ':' -f 1); do
+    new_password=$(openssl rand -base64 12)
+    echo $new_password | passwd $user
+    
+    if [ $? -eq 0 ]; then
+      echo "$user,$password"
+    else
+      echo "Failed to change password for $user"
+    fi
+  done
+else
+  for user in $(cat /etc/passwd | grep "sh$" | cut -d ':' -f 1); do
+    # Check if user is not special
     echo $special | grep "$user " > /dev/null || echo $special | grep "$user$" > /dev/null
     if [ $? -ne 0 ]; then
-        # Generate a random password for each user
-        pword=$(head -c 200 /dev/urandom | tr -dc 'a-zA-Z0-9@$%&!?:+-=' | cut -c1-16)
+      # Generate a random password for each user
+      pword=$(head -c 200 /dev/urandom | tr -dc 'a-zA-Z0-9@$%&!?:+-=' | cut -c1-16)
 
-        # Change password and echo to std output for use in csv file
-        echo "$user:$pword" | $passChanger > /dev/null
-        echo "$user,$pword"
+      # Change password and echo to std output for use in csv file
+      echo "$user:$pword" | $passChanger > /dev/null
+      echo "$user,$pword"
     fi
-done
+  done
 echo "Passwords successfully changed.  Submit to black team"
