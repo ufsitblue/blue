2ba3915c323514a027d0cb86ca9de41f0e2999f4
Ontu <73860587+0ntu@users.noreply.github.com>
2025-02-14 02:39:48 -0500

    Ansible python: add support for more pkg mgrs

--- ccdc_2025/ansible/playbooks/python/files/install_python3.sh
+++ ccdc_2025/ansible/playbooks/python/files/install_python3.sh
@@ -4,11 +4,11 @@
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     case "$ID" in
-        ubuntu|debian)
+        ubuntu|debian|kali)
             apt update && apt install -y python3
             exit $?
             ;;
-        rocky|centos|rhel)
+        rocky|centos|rhel|fedora)
             yum install -y python3
             exit $?
             ;;
@@ -18,6 +18,26 @@ if [ -f /etc/os-release ]; then
             ;;
         alpine)
             apk add --no-cache python3
+            exit $?
+            ;;
+        opensuse|sles|opensuse-leap)
+            zypper install -y python3
+            exit $?
+            ;;
+        gentoo)
+            emerge --ask dev-lang/python
+            exit $?
+            ;;
+        void)
+            xbps-install -Sy python3
+            exit $?
+            ;;
+        slackware)
+            slackpkg install python3
+            exit $?
+            ;;
+        nixos)
+            nix-env -iA nixpkgs.python3
             exit $?
             ;;
         *)
