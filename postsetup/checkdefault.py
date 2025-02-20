d8548c07644ea0bc899bc2b71f2f2581b02488df
yhuang3-uf <115114652+yhuang3-uf@users.noreply.github.com>
2025-02-19 22:28:58 -0500

    Update the script again

--- postsetup/checkdefault.py
+++ postsetup/checkdefault.py
@@ -6,6 +6,7 @@ import argparse
 import configparser
 import csv
 import pathlib
+import random
 import sys
 import time
 import typing
@@ -18,8 +19,8 @@ import plankaapi
 
 def main(argv: list[str]) -> int:
     argparser: argparse.ArgumentParser = argparse.ArgumentParser(description="Checks for default credentials on machines")
-    argparser.add_argument("--delay-time", "-d", default=5.0, type=float,
-            help="The minimum delay between requests.")
+    argparser.add_argument("--delay-time", "-d", default=2.0, type=float,
+            help="The minimum delay between requests. Defaults to 2.")
     argparser.add_argument("--endpoint", "-e", default=None, type=str,
             help="The HTTP endpoint to send warnings to")
     argparser.add_argument("--planka-api", "-p", action="store_true",
@@ -82,7 +83,7 @@ def main(argv: list[str]) -> int:
                     """The time that the next request should start by"""
                     ssh_connection = fabric.Connection(host=ip_address, port=port, user=user, connect_kwargs={
                         "password": password,
-                        "timeout": 8
+                        "timeout": 5
                     })
                     try:
                         ssh_connection.run("id")
@@ -95,15 +96,19 @@ def main(argv: list[str]) -> int:
                                             "X-Priority": "urgent",
                                             "X_Tags": "defaultcreds,critical",
                                         })
-                                if planka_list is not None:
-                                    planka_list.create_card("DEFAULT PASSWORD DETECTED ON " + ip_address + " PORT " + str(port) + 
-                                                            " WITH USER " + user + " PASSWORD " + password)
                             except requests.RequestException as e:
                                 print("WARN: An exception occured when publishing webhook: " + str(e))
+                        if planka_list is not None:
+                            planka_list.create_card("DEFAULT PASSWORD DETECTED ON " + ip_address + " PORT " + str(port) + 
+                                    " WITH USER " + user + " PASSWORD " + password, random.randint(1000, 9000))
+                    except paramiko.ssh_exception.NoValidConnectionsError:
+                        print("WARN: SSH connection refused " + ip_address + ":" + str(port))
                     except TimeoutError:
-                        print("Timed out")
+                        print("WARN: SSH connection timed out " + ip_address + ":" + str(port))
                     except paramiko.ssh_exception.AuthenticationException:
                         print("Authentication failure")
+                    except Exception as e:
+                        print("ERROR: Got unexpected exception: " + str(e))
 
                     # Wait until whenever we should send the next request.
                     time.sleep(max(0, next_request_start_time - time.time()))
